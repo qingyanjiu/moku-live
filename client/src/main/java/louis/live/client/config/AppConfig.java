4ce14129a9c72ad6393c47a71e6f62ce36b7ab5c
louisliu <louisliu@cisco.com>
2019-07-04 09:21:56 +0800

    config client not to use apollo config center

--- client/src/main/java/louis/live/client/config/AppConfig.java
+++ client/src/main/java/louis/live/client/config/AppConfig.java
@@ -4,7 +4,7 @@ import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableApolloConfig
+//@EnableApolloConfig
 public class AppConfig {
 }
 
