06e93547589cdc140321e925d1406a1a72cb039f
louisliu <louisliu@cisco.com>
2019-07-05 09:25:37 +0800

    added refresh scope to live controller

--- client/src/main/java/moku/live/client/controller/LiveController.java
+++ client/src/main/java/moku/live/client/controller/LiveController.java
@@ -6,6 +6,7 @@ import moku.live.client.vo.LiveInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/live")
-//@RefreshScope
+@RefreshScope
 public class LiveController {
 
     @Autowired
